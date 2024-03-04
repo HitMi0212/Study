c1a3f2b7df75e0fca3b103e93af2525808c43403
Seung Jun <pache31@naver.com>
2024-03-04 20:12:08 +0900

    [Fix] 게시글 업로드 - console 삭제 -  정규표현식 수정

--- nodejs-sns/controllers/post.js
+++ nodejs-sns/controllers/post.js
@@ -15,14 +15,15 @@ exports.uploadPost = async (req, res, next) => {
             UserId: req.user.id,
         });
         // await post.addUser(req.user.id)
-        const hashtags = req.body.content.match(/#[$\s#]*/g);
-        if(hashtags){
-            const result = await Promise.all(hashtags.map((tag) => {
+        const hashtags = req.body.content.match(/#[^\s#]*/g);
+        if (hashtags) {
+            const result = await Promise.all(
+              hashtags.map(tag => {
                 return Hashtag.findOrCreate({
-                    where: { title: tag.slice(1).toLowerCase() }
-                });
-            }));
-            console.log('result', result);
+                  where: { title: tag.slice(1).toLowerCase() },
+                })
+              }),
+            );
             await post.addHashtags(result.map(r => r[0]));
         }
         res.redirect('/');
