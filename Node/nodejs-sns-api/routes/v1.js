7c0e4b2f250d7fcb41c72212c2378264d3c7216c
Seung Jun <pache31@naver.com>
2024-03-09 14:14:29 +0900

    [Feat] API서버 만들기

--- Node/nodejs-sns-api/routes/v1.js
+++ Node/nodejs-sns-api/routes/v1.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { verifyToken } = require('../middlewares');
-const { createToken, tokenTest } = require('../controllers/v1');
+const { createToken, tokenTest, getMyPosts, getPostsByHashtag } = require('../controllers/v1');
 
 const router = express.Router();
 
 // /v1/token
 router.post('/token', createToken); // req.body.clientSecret
 router.get('/test', verifyToken, tokenTest);
+
+// GET /v1/posts/my
+router.get('/posts/my', verifyToken, getMyPosts);
+// GET /v1/posts/hashtag/:title
+router.get('/posts/hashtag/:title', verifyToken, getPostsByHashtag);
 
 module.exports = router;
