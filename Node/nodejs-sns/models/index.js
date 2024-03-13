90cb324664e409b8e3bcfb2861092340f64f3dcf
Seung Jun <pache31@naver.com>
2024-03-13 20:54:02 +0900

    [Test] User model test (커버리지용) - 커버리지를 채우기 위한 테스트는 지양

--- Node/nodejs-sns/models/index.js
+++ Node/nodejs-sns/models/index.js
@@ -21,6 +21,7 @@ fs
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
+      !file.includes('test') &&
       file !== basename &&
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
