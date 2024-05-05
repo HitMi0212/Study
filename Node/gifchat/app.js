0f585043b7f968d6e2f9118cd27664e593bf4b14
Seung Jun <pache31@naver.com>
2024-05-05 22:08:58 +0900

    [Feat] 1. 세션 미들웨어 생성 2. 방 참가/퇴장 로직 추가

--- Node/gifchat/app.js
+++ Node/gifchat/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -34,7 +34,8 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+});
+app.use(sessionMiddleware);
 
 app.use((req, res, next) => {
   if(!req.session.color) {
@@ -63,4 +64,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleware);
